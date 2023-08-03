1efc7c131a2e5e7ba3a6eb94ed49fa24500ab327
gongahkia <gong@gongahkias-MacBook-Pro.local>
2023-08-04 01:19:34 +0800

    aight bet fam

---
+++ browser-game/canvas.js
@@ -0,0 +1,59 @@
+// -- instantiating the HTML5 canvas
+
+var canvas = document.getElementById("theCanvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// -- testing the drawing the player character
+
+// required variables
+var c = canvas.getContext('2d');
+var x = innerWidth/2;
+var y = innerHeight/2;
+var radius = 10;
+var colorArray = ['blue', 'red', 'yellow', 'green', 'pink', 'cyan', 'purple', 'orange', 'black']
+var color = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+// actual drawing methods
+c.beginPath(); 
+c.arc(x,y,radius,0,Math.PI * 2, false);
+c.strokeStyle = color;
+c.stroke();
+c.fillStyle = color;
+c.fill();
+
+// -----
+
+
+// -- player class
+// * add detection of player input and figure out the integration of objects later
+// * integrate player input and player movement into the character controller
+
+/* var colorArray = ['blue', 'red', 'yellow', 'green', 'pink', 'cyan', 'purple', 'orange', 'black']
+var color = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+function player(x:number,y:number,radius:number) {
+    
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+
+    this.draw() {
+        c.beginPath();
+        c.arc(x,y,radius,Math.PI * 2, false);
+        c.strokeStyle = color;
+        c.fillStyle = color;
+        c.stroke();
+        c.fill();
+    };
+
+    this.update() {
+        this.draw();
+    }
+
+} */
+
+// testing the instantiating of the player character 
+// * continue working on this one
+
+// var player1 = new player(innerWidth/2, innerHeight/2, 10);
