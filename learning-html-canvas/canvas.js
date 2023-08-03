ff971a00dae4ccac9b444eb29d2906fd67712b4b
gongahkia <gong@gongahkias-MacBook-Pro.local>
2023-08-03 16:47:12 +0800

    a lot of new stuff learned

--- learning-html-canvas/canvas.js
+++ learning-html-canvas/canvas.js
@@ -18,7 +18,7 @@ var c = canvas.getContext('2d');
 // c.fillStyle = 'rgba(255, 0, 0, 0.5)'
 
 // creates a line in canvas
-c.beginPath();
+/* c.beginPath();
 var triangleStartingXCoord = Math.random() * window.innerWidth;
 var triangleStartingYCoord = Math.random() * window.innerHeight;
 c.moveTo(triangleStartingXCoord, triangleStartingYCoord);
@@ -26,7 +26,7 @@ c.lineTo(Math.random() * window.innerWidth, Math.random() * window.innerHeight);
 c.lineTo(Math.random() * window.innerWidth, Math.random() * window.innerHeight);
 c.lineTo(triangleStartingXCoord, triangleStartingYCoord);
 c.strokeStyle = "blue";
-c.stroke();
+c.stroke(); */
 
 // drawing a circle
 // c.beginPath(); // the beginPath() method restarts the coordinate pointer for the canvas context object
@@ -34,7 +34,7 @@ c.stroke();
 // c.stroke();
 
 // drawing multiple circles using a for loop and the random coordinate determiner
-for (var i = 0; i < 10; i++) {
+/* for (var i = 0; i < 10; i++) {
 
     var color_weight = Math.random();
     if (color_weight <= 0.25) {
@@ -58,4 +58,62 @@ for (var i = 0; i < 10; i++) {
     c.strokeStyle = color;
     c.stroke();
 
+}*/
+
+// 1. FIGURE OUT HOW TO MAKE CIRCLE COLOR CHANGE AND MAINTAIN EVERY SINGLE TIME IT HITS A WALL
+
+
+// a javascript 'object' is called with the `function` name call
+function Circle(x,y,dx,dy,circleRadius) {
+
+    // Circle object attributes
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.circleRadius = circleRadius;
+
+    // Circle object methods
+    this.draw = function() {
+        console.log("circle has been instantiated and drawn");
+        c.beginPath();
+        c.arc(this.x,this.y,this.circleRadius,0,Math.PI * 2, false);
+        c.strokeStyle = 'blue';
+        c.stroke();
+    }
+
+    // Another Circle object method
+    this.update = function(){
+        if (this.x + this.circleRadius > innerWidth || this.x - this.circleRadius < 0) {
+            this.dx = -this.dx;
+        }
+        
+        if (this.y + this.circleRadius > innerHeight || this.y - this.circleRadius < 0) { 
+            this.dy = -this.dy;
+        }
+        this.x += this.dx; // currently, X velocity is 4 pixel/second
+        this.y += this.dy; // currently  y velocity is 4 pixel/second
+    }
 }
+
+// instantiating a new instance of the Circle object
+var circle = new Circle(200,200,5,5,30);
+
+// main animation function loop refreshes the page repeatedly to simulate animation as the coordinates change
+var x = Math.random() * innerWidth;
+var y = Math.random() * innerHeight;
+var dx = (Math.random() - 0.5) * 20;
+var dy = (Math.random() - 0.5) * 20;
+var circleRadius = 30; // determined by drawing of arc 
+
+// animate() function
+function animate() {
+    requestAnimationFrame(animate); // recursive function call
+
+    // we clear the screen first, then draw the circle, and only then update its location and redraw
+    c.clearRect(0,0, innerWidth, innerHeight);
+    circle.draw();
+    circle.update();
+}
+
+animate();
