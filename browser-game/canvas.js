3fec0b3d3bae26af3394b5d148ff6ca0f5e50e19
gongahkia <angryapplegravy@gmail.com>
2023-08-04 12:33:55 +0800

    wah shit

--- browser-game/canvas.js
+++ browser-game/canvas.js
@@ -1,3 +1,5 @@
+// * debug this entire document lmao
+
 // -- instantiating the HTML5 canvas
 
 var canvas = document.getElementById("theCanvas");
@@ -8,51 +10,82 @@ canvas.height = window.innerHeight;
 
 // required variables
 var c = canvas.getContext('2d');
-var x = innerWidth/2;
-var y = innerHeight/2;
-var radius = 10;
-var colorArray = ['blue', 'red', 'yellow', 'green', 'pink', 'cyan', 'purple', 'orange', 'black']
-var color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
-// actual drawing methods
-c.beginPath(); 
-c.arc(x,y,radius,0,Math.PI * 2, false);
-c.strokeStyle = color;
-c.stroke();
-c.fillStyle = color;
-c.fill();
+// keyPress event object
+var keyPress = {
+    key: undefined,
+    keyCode: undefined
+}
 
-// -----
+window.addEventListener('keydown', 
+    function() {
+    keyPress.key = event.key;
+    keyPress.keyCode = event.code;
+    console.log(event);
+})
 
 
 // -- player class
 // * add detection of player input and figure out the integration of objects later
 // * integrate player input and player movement into the character controller
 
-/* function player(x:number,y:number,radius:number) {
+var colorArray = ['blue', 'red', 'yellow', 'green', 'pink', 'cyan', 'purple', 'orange', 'black']
+
+function Player(x,y,radius) {
     
+    // player object attributes
     this.x = x;
     this.y = y;
     this.radius = radius;
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
-    this.draw() {
-        var colorArray = ['blue', 'red', 'yellow', 'green', 'pink', 'cyan', 'purple', 'orange', 'black']
-        var color = colorArray[Math.floor(Math.random() * colorArray.length)];
+    // drawing method
+    this.draw() = function() {
+        console.log("player circle has been instantiated");
         c.beginPath();
-        c.arc(x,y,radius,Math.PI * 2, false);
-        c.strokeStyle = color;
+        c.arc(this.x,this.y,this.radius,Math.PI * 2, false);
+        c.strokeStyle = this.color;
         c.stroke();
-        c.fillStyle = color;
+        c.fillStyle = this.color;
         c.fill();
-    };
-
-    this.update() {
-        this.draw();
     }
 
-} */
+    // update method
+    this.update() = function() {
+        // * integrate this portion into the main update() loop later
+        console.log(keyPress.key);
+        switch(keyPress.key) {
+            case 'w':
+                this.y -= 1;
+            case 'W':
+                this.y -= 1;
+            case 's':
+                this.y += 1;
+            case 'S':
+                this.y += 1;
+            case 'a':
+                this.x -= 1;
+            case 'A':
+                this.x -= 1;
+            case 'd':
+                this.x += 1;
+            case 'D':
+                this.x += 1;
+        }
+        this.draw();
+        console.log(this.x, this.y);
+    }
 
-// testing the instantiating of the player character 
-// * continue working on this one
+}
 
-// var player1 = new player(innerWidth/2, innerHeight/2, 10);
+var player1 = new Player(innerWidth/2, innerHeight/2, 10);
+
+// actual animation loop
+
+function animate() {
+    requestAnimationFrame(animate); // recursive function
+    c.clearRect(0,0,innerWidth,innerHeight);
+    player1.update();
+}
+
+animate();
